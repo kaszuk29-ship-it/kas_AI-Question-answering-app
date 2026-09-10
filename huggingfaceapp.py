76fbe2473e4ded3dca45699bf98e4934fc8aecb0
kaszuk29-ship-it <kaszuk29@gmail.com>
2026-09-10 16:02:33 +0530

    Add files via upload

--- huggingfaceapp.py
+++ huggingfaceapp.py
@@ -6,15 +6,19 @@ st.set_page_config(
     page_title="Kas AI App",
     page_icon="🤖"
 )
+
 st.title("🤖 Kas AI - Question & Answer")
 st.write("Ask a question and get an AI-generated answer!")
+
 question = st.text_area(
     "❓ Enter your question:",
     placeholder="Example: What is photosynthesis?"
 )
+
 client = InferenceClient(
     api_key=os.environ["token"]
 )
+
 if st.button("✨ Ask Kas AI"):
 
     if question:
